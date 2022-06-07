a0256f80564ce8209f139aca0b3b5d74e969e78a
Anne Bortoli <annebortoli@gmail.com>
2022-06-07 10:20:44 -0300

    :sparkles: added backToTop button

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
+/* Outra opção, nesse caso pode tirar da tag body o  onscroll="onScroll()" */
+//window.addEventListener('scroll', onScroll);
+/* é preciso executar uma única vez */
+//onScroll();
+
 const onScroll = () => {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+};
+
+const showNavOnScroll = () => {
   const navigation = document.getElementById('navigation');
   return scrollY > 0
     ? navigation.classList.add('scroll')
     : navigation.classList.remove('scroll');
+};
+
+const showBackToTopButtonOnScroll = () => {
+  const backToTopButton = document.getElementById('backToTopButton');
+  return scrollY > 550
+    ? backToTopButton.classList.add('show')
+    : backToTopButton.classList.remove('show');
 };
 
 const openMenu = () => {
